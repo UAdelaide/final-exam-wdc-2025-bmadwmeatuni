504dd2393f64f8deaf40610cee3b82d8aa4d5952
bmadwmeatuni <a1895379@adelaide.edu.au>
2025-06-20 15:38:34 +0000

    [automatic commit] 2025-06-20 15:38:34 UTC 193.115.123.224

--- part1/query.sql
+++ part1/query.sql
@@ -8,6 +8,6 @@ VALUES ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')
 INSERT INTO WalkRequests (requested_time, duration_minutes, status, location, dog_id)
 VALUES ('2025-06-10 08:00:00', 30, 'open', 'Parklands', (SELECT dog_id FROM Dogs WHERE name = 'Max')),
 ('2025-06-10 09:30:00', 45, 'accepted', 'Beachside Ave', (SELECT dog_id FROM Dogs WHERE name = 'Bella')),
-('2025-06-10 10:00:00', 30, 'open', 'Parklands', (SELECT dog_id FROM Dogs WHERE name = 'Max')),
+('2025-06-10 10:00:00', 60, 'open', 'Parklands', (SELECT dog_id FROM Dogs WHERE name = 'Luna')),
 ('2025-06-10 23:45:00', 30, 'open', 'Parklands', (SELECT dog_id FROM Dogs WHERE name = 'Max')),
 ('2025-06-10 00:00:05', 30, 'open', 'Parklands', (SELECT dog_id FROM Dogs WHERE name = 'Max'));
