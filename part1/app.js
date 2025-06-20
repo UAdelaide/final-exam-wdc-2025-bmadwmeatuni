659c860ae37e14d63534fdd3d8f61c3e601ba60b
bmadwmeatuni <a1895379@adelaide.edu.au>
2025-06-20 19:18:47 +0000

    [automatic commit] 2025-06-20 19:18:47 UTC 193.115.123.224

--- part1/app.js
+++ part1/app.js
@@ -117,7 +117,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
   }
 });
 
-app.get('/api/walkrequests/open', async (req, res) => {
+app.get('/api/walkers/summary', async (req, res) => {
   try {
     const [open] = await db.execute(`
         SELECT r.request_id AS request_id, d.name AS dog_name, r.requested_time AS requested_time, r.duration_minutes AS duration_minutes, r.location AS location, u.username AS owner_username
