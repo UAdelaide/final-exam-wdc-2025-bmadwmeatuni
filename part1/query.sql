eb7ff8159345a474985e37f1f2a0eebced8a2b35
bmadwmeatuni <a1895379@adelaide.edu.au>
2025-06-20 15:18:43 +0000

    [automatic commit] 2025-06-20 15:18:43 UTC 193.115.123.224

--- part1/query.sql
+++ part1/query.sql
@@ -5,7 +5,7 @@ VALUES ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')
 ('Bow', 'medium', (SELECT user_id FROM Users WHERE username = 'spongebob')),
 ('Mayonnaise', 'small', (SELECT user_id FROM Users WHERE username = 'spongebob'));
 
-INSERT INTO WalkRequests (name, size, owner_id)
+INSERT INTO WalkRequests (name, size, dog_id)
 VALUES ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
 ('Bella', 'small', (SELECT user_id FROM Users WHERE username = 'carol123')),
 ('Luna', 'large', (SELECT user_id FROM Users WHERE username = 'carol123')),
