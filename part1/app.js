a0078f1daf984d9903ee9c3b5332b084789dec08
bmadwmeatuni <a1895379@adelaide.edu.au>
2025-06-20 19:10:51 +0000

    [automatic commit] 2025-06-20 19:10:51 UTC 193.115.123.224

--- part1/app.js
+++ part1/app.js
@@ -105,7 +105,7 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
   try {
     const [open] = await db.execute(`
-        SELECT r.request_id AS request_id, r.requested_time AS requested_
+        SELECT r.request_id AS request_id, r.requested_time AS requested_time, r.duration_minutes AS duration_minutes,
         FROM WalkRequests r
         WHERE r.status = 'open'
         `);
