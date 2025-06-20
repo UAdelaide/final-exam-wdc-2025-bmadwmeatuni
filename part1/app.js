c5b5b1225a5bcde7895beb53b95f2c923fe09dfc
bmadwmeatuni <a1895379@adelaide.edu.au>
2025-06-20 18:52:20 +0000

    [automatic commit] 2025-06-20 18:52:20 UTC 193.115.123.224

--- part1/app.js
+++ part1/app.js
@@ -67,10 +67,10 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/api/dogs', async (req, res) => {
-    try {
+ {
     const [users] = await db.execute('SELECT * FROM Users');
     res.json(users);
-  } catch (err) {
+    } catch (err) {
     res.status(500).json({ error: 'Failed to fetch Users' });
   }
 })
