efba814201f4a4015622d9010d73837580648f5e
bmadwmeatuni <a1895379@adelaide.edu.au>
2025-06-20 18:15:06 +0000

    [automatic commit] 2025-06-20 18:15:06 UTC 193.115.123.224

--- part1/app.js
+++ part1/app.js
@@ -46,7 +46,7 @@ let db;
     });
 
     // Insert data if table is empty
-    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
+    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
     if (rows[0].count === 0) {
       await db.execute(`
         INSERT INTO books (title, author) VALUES
