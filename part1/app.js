5827b81a126f83017800d21b58ee94ac58408f43
bmadwmeatuni <a1895379@adelaide.edu.au>
2025-06-20 17:58:55 +0000

    [automatic commit] 2025-06-20 17:58:55 UTC 193.115.123.224

--- part1/app.js
+++ part1/app.js
@@ -4,8 +4,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql2/promise');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//  var indexRouter = require('./routes/index');
+//  var usersRouter = require('./routes/users');
 
 var app = express();
 
