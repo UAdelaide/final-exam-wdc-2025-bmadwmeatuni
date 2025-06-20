889bc455ad14278e4ccdefcdfe7f510b9537889a
bmadwmeatuni <a1895379@adelaide.edu.au>
2025-06-20 18:16:07 +0000

    [automatic commit] 2025-06-20 18:16:07 UTC 193.115.123.224

--- part1/app.js
+++ part1/app.js
@@ -49,10 +49,16 @@ let db;
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
+        INSERT INTO Users (username, email, password_hash, role)
+        VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+
+('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+
+('carol123', 'carol@example.com', 'hashed789', 'owner'),
+
+('patrick', 'star@sb.com', 'patrickstar', 'walker'),
+
+('spongebob', 'sb@sb.com', 'bikini', 'owner')
       `);
     }
   } catch (err) {
