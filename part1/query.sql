9c6e6a5dc8d580d96ea6d63c5611430a6e9b7d6d
bmadwmeatuni <a1895379@adelaide.edu.au>
2025-06-20 15:12:04 +0000

    [automatic commit] 2025-06-20 15:12:04 UTC 193.115.123.224

--- part1/query.sql
+++ part1/query.sql
@@ -1,4 +1,5 @@
 INSERT INTO Dogs (name, size, owner_id)
 VALUES ('Max', 'medium', SELECT user_id FROM Users WHERE username = 'alice123'),
 ('Bella', 'small', SELECT user_id FROM Users WHERE username = 'carol123'),
-('Luna', 'small', SELECT user_id FROM Users WHERE username = 'carol123'),
+('Luna', 'large', SELECT user_id FROM Users WHERE username = 'carol123'),
+('Bella', 'small', SELECT user_id FROM Users WHERE username = 'carol123'),
