3c7923355850dba74c86deb6b71ea802ac3df9c5
bmadwmeatuni <a1895379@adelaide.edu.au>
2025-06-21 06:14:04 +0000

    [automatic commit] 2025-06-21 06:14:04 UTC 193.115.123.224

--- part2/app.js
+++ part2/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const db = require('../models/db');
-var mysql = require('mysql2/promise');
 
 const app = express();
 
