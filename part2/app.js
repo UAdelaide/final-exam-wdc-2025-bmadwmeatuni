4689bf780531ae1e432e7220f5eadb899bf7fdd5
bmadwmeatuni <a1895379@adelaide.edu.au>
2025-06-21 06:12:01 +0000

    [automatic commit] 2025-06-21 06:12:01 UTC 193.115.123.224

--- part2/app.js
+++ part2/app.js
@@ -31,7 +31,7 @@ app.use('/api/users', userRoutes);
 app.get('/api/dogs', async (req, res) => {
   try {
     const [dogs] = await db.execute(`
-        SELECT Dogs.name AS dog_name, Dogs.size AS size, Users.username AS owner_username
+        SELECT Dogs.dog_id, Dogs.name AS dog_name, Dogs.size AS size, Users.username AS owner_username
         FROM Dogs JOIN Users ON Dogs.owner_id = Users.user_id
         `);
     res.json(dogs);
