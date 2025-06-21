53585ccb66a2c00d687a452f9fdaa8e229a31de0
bmadwmeatuni <a1895379@adelaide.edu.au>
2025-06-21 03:54:32 +0000

    [automatic commit] 2025-06-21 03:54:32 UTC 193.115.123.224

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -220,7 +220,7 @@ function logout(){
 
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            window.location.href = 'index.html'; //
+            window.location.href = 'index.html'; // Redirecting to the log in page after logout
         }
     };
 
