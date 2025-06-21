1eadda445a757a1067ad8fb405d503dd8a4df598
bmadwmeatuni <a1895379@adelaide.edu.au>
2025-06-21 02:41:47 +0000

    [automatic commit] 2025-06-21 02:41:47 UTC 193.115.123.224

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -187,7 +187,8 @@ function login(){
     // Define function to run on response
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            alert("Welcome "+this.responseText);
+            const response = JSON.parse(this.responseText);
+            
         } else if (this.readyState == 4 && this.status >= 400) {
             alert("Login failed");
         }
