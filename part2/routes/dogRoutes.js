7a06e34cf38021d6d19da51ade7d0da444314527
bmadwmeatuni <a1895379@adelaide.edu.au>
2025-06-21 07:01:47 +0000

    [automatic commit] 2025-06-21 07:01:47 UTC 193.115.123.224

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models/db');
+
 router.get('/', async (req, res) => {
   try {
     const [dogs] = await db.execute(`
@@ -9,3 +13,5 @@ router.get('/', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch dogs' });
   }
 });
+
+module.exports = router;
